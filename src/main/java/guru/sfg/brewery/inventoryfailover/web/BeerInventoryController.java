bc576e87971f83932715164e1705b484483f360e
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2021-12-07 13:33:10 +0100

    Circuit breaker

--- src/main/java/guru/sfg/brewery/inventoryfailover/web/BeerInventoryController.java
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/BeerInventoryController.java
@@ -3,10 +3,13 @@ package guru.sfg.brewery.inventoryfailover.web;
 import guru.sfg.brewery.inventoryfailover.model.BeerInventoryDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -15,16 +18,16 @@ import java.util.UUID;
 public class BeerInventoryController {
 
     @GetMapping("/inventory-failover")
-    BeerInventoryDto inventoryFailover(){
-        BeerInventoryDto dto = BeerInventoryDto.builder()
+    public ResponseEntity<List<BeerInventoryDto>> inventoryFailover(){
+        List<BeerInventoryDto> beerInventoryDtoList = List.of(BeerInventoryDto.builder()
                 .id(UUID.randomUUID())
                 .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
                 .quantityOnHand(999)
                 .createdDate(OffsetDateTime.now())
                 .lastModifiedDate(OffsetDateTime.now())
-                .build();
+                .build());
 
-        log.info("Finding Inventory for dto: " + dto);
-        return dto;
+        log.info("Finding Inventory for dto: " + beerInventoryDtoList);
+        return new ResponseEntity<>(beerInventoryDtoList, HttpStatus.OK);
     }
 }
